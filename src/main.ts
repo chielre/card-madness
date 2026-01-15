e53cb5cbf46ed0c4a1ed0f8dbc102cf4d8bd7b4f
Chiel Reijnen <zakelijk@chielreijnen.nl>
2026-01-15 03:59:38 +0100

    Str utilities

--- src/main.ts
+++ src/main.ts
@@ -4,6 +4,8 @@ import App from './App.vue'
 import router from './router/index'
 import './styles/base.css' 
 
+import { strLimit } from '@/utils/str'
+
 const app = createApp(App)
 
 app.use(createPinia())
