a78f95992252e1638f2fca6747d2fbd523c61061
Chiel Reijnen <zakelijk@chielreijnen.nl>
2026-06-12 22:29:19 +0200

    Card packs will not show the CM logo if a pack is not an official pack

--- src/components/CardPack.vue
+++ src/components/CardPack.vue
@@ -15,6 +15,10 @@ const openInfo = () => {
     emit('show-info', props.pack.id)
 }
 
+const isOfficialPack = () => {
+    return props.pack.id.startsWith('cm_');
+}
+
 </script>
 
 <template>
@@ -25,29 +29,32 @@ const openInfo = () => {
         </div>
 
 
-        <button v-if="!selected" type="button" class="group-hover:translate-x-0 group-hover:translate-y-0 hover:bg-white hover:text-black delay-100 translate-x-20 -translate-y-20  transition-transform   z-10 absolute outline-1 -outline-offset-4 top-3 right-3 w-8 h-8 rounded-full text-xl bg-black  text-white  flex items-center justify-center" @click.stop="openInfo">
+        <button type="button" class="hover:bg-white hover:text-black transition-transform z-30 absolute outline-1 -outline-offset-4 top-3 right-3 w-8 h-8 rounded-full text-xl bg-black text-white flex items-center justify-center" :class="selected ? 'translate-x-0 translate-y-0' : 'group-hover:translate-x-0 group-hover:translate-y-0 delay-100 translate-x-20 -translate-y-20'" @click.stop="openInfo">
             <information-symbol />
         </button>
 
         <div class="pack-background absolute top-[50%] left-[50%] translate-x-[-50%] translate-y-[-50%] w-[150%] h-[150%] bg-center bg-repeat opacity-60 pointer-events-none -rotate-12" :style="{ backgroundImage: `url(${pack.bgUrl})`, backgroundSize: '100px' }"></div>
         <div class="relative text-white font-bold flex flex-col h-full items-center justify-between w-full ">
 
-            <div v-if="pack.deprecated" class="group-hover:scale-0  transition duration-250 absolute bottom-0 left-0 text-xs right-0 z-10 bg-red-600 border-2 border-b-4 shadow-lg py-1 px-3 rotate-6 border-red-700 text-center rounded-xl">
+            <div v-if="pack.deprecated" class="group-hover:scale-0  transition duration-250 absolute bottom-0 left-0  right-0 z-10 bg-red-600 border-2 border-b-4 shadow-lg py-1 px-3 rotate-1 border-red-800 text-center rounded-xl">
                 Leaving Soon
             </div>
 
-            <div class="relative  group-hover:scale-110 transition duration-250">
-                <img width="180" :src="pack.logoUrl">
+            <div class="relative mt-8 group-hover:scale-110 transition duration-250">
+                <img width="100%" class=" " :src="pack.logoUrl">
                 <img v-if="pack.nsfw" width="50" src="../assets/images/pack_nsfw.png" class=" absolute bottom-2 left-3 rotate-12" alt="">
             </div>
 
             <div class="group-hover:translate-y-24 group-hover:scale-0 transition duration-250">
                 <div v-if="pack.partnerUrl" class="flex gap-2">
-                    <img class="" width="75" src="../assets/images/logo.png" alt="">
+                    <div v-if="isOfficialPack()">
+                        <img class="" width="75" src="../assets/images/logo.png" alt="">
+                    </div>
+
                     <img class="" width="75" :src="pack.partnerUrl" alt="">
                 </div>
 
-                <div v-else>
+                <div v-else-if="isOfficialPack()">
                     <img class="" width="75" src="../assets/images/logo.png" alt="">
                 </div>
             </div>
